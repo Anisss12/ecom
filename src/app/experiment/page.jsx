52602cac575eb73385864350b8853fb591d70995
Anisss12 <151550400+Anisss12@users.noreply.github.com>
2025-03-02 23:05:46 +0530

    Add files via upload

---
+++ src/app/experiment/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Filter from "../filter/Filter";
+
+const page = () => {
+  return (
+    <div>
+      <Filter></Filter>
+    </div>
+  );
+};
+
+export default page;
